27cc94547963484e87d829243df09da990d14ccb
scrieriortodoxe <none>
2018-03-30 12:21:25 +0200

    first release

---
+++ books.js
@@ -0,0 +1,14 @@
+var BOOKS = [
+{ "all":"@",
+  "title": "Carte de rugaciuni",
+  "author": "Atanasie",
+  "year": "2011",
+  "pdf": "ur1",
+  "other": "some url"},
+{ "all":"@",
+  "title": "Carte de rugaciuni",
+  "author": "Atanasie",
+  "year": "2011",
+  "pdf": "ur1",
+  "other": "some other url"},
+]
